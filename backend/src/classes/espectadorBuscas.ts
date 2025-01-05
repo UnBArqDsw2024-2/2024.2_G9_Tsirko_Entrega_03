c1aea7d8602145d0ecc3652b58fbd819139a2b06
João Pedro <joaopedrosilvarodrigues57@gmail.com>
2025-01-05 14:50:50 -0300

    merge

--- backend/src/classes/espectadorBuscas.ts
+++ backend/src/classes/espectadorBuscas.ts
@@ -2,13 +2,7 @@ import { EstrategiaDeBusca } from "./estrategiaDeBusca";
 import { Evento } from "./evento";
 
 export class BuscaPorNome implements EstrategiaDeBusca {
-<<<<<<< HEAD
-    public buscar(eventos: Evento[], filtro: string): Evento[] {
-      return eventos.filter(evento => evento.getNome().includes(filtro));
-    }
-  }
-  
-=======
+
   public buscar(eventos: Evento[], filtro: string): Evento[] {
     return eventos.filter((evento) => evento.getNome().includes(filtro));
   }
@@ -22,4 +16,3 @@ export class BuscaPorData implements EstrategiaDeBusca {
     );
   }
 }
->>>>>>> 5125cd0aab406e86ed232c44a64239064eb91791
