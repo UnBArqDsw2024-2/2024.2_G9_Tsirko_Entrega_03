5125cd0aab406e86ed232c44a64239064eb91791
magnluiz <55704216+magnluiz@users.noreply.github.com>
2025-01-05 14:41:45 -0300

    commit individual - Magno

--- backend/src/classes/espectadorBuscas.ts
+++ backend/src/classes/espectadorBuscas.ts
@@ -1,9 +1,17 @@
-import { EstrategiaDeBusca } from './estrategiaDeBusca';
-import { Evento } from './evento';
-
+import { EstrategiaDeBusca } from "./estrategiaDeBusca";
+import { Evento } from "./evento";
 
 export class BuscaPorNome implements EstrategiaDeBusca {
-    public buscar(eventos: Evento[], filtro: string): Evento[] {
-      return eventos.filter(evento => evento.getNome().includes(filtro));
-    }
+  public buscar(eventos: Evento[], filtro: string): Evento[] {
+    return eventos.filter((evento) => evento.getNome().includes(filtro));
   }
+}
+
+export class BuscaPorData implements EstrategiaDeBusca {
+  public buscar(eventos: Evento[], filtro: string): Evento[] {
+    const dataFiltro = new Date(filtro);
+    return eventos.filter(
+      (evento) => evento.getData().toDateString() === dataFiltro.toDateString()
+    );
+  }
+}
