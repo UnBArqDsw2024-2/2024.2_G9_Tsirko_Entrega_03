351ec4e83db1c41267e16e610c3924c93ebecff0
TheCarlosRamos <116928666+TheCarlosRamos@users.noreply.github.com>
2025-01-06 21:32:59 -0300

    Adicionando ajuste alternativo para o método accept

--- backend/src/classes/eventoComposite.ts
+++ backend/src/classes/eventoComposite.ts
@@ -29,3 +29,39 @@ export class EventoComposite implements ComponenteEvento {
         this.eventos.forEach(evento => evento.accept(visitor));
     }
 }
+
+
+/*
+Implementação alternativa que segue uma abordagem para lidar 
+com o Visitor em uma coleção de eventos eliminando a 
+necessidade de verificações dinâmicas como instanceof:
+*\
+
+/*
+import { Evento } from './evento';
+
+export class EventoComposite extends Evento {
+    private eventos: Evento[] = [];
+
+    constructor(nome: string, descricao: string) {
+        super(nome, descricao, new Date(), ""); 
+    }
+
+    addEvento(evento: Evento): void {
+        this.eventos.push(evento);
+    }
+
+    getEventos(): Evento[] {
+        return this.eventos;
+    }
+
+    accept(visitor: EventoVisitor): void {
+        visitor.visitComposite(this);
+    }
+
+    exibirDetalhes(): void {
+        console.log(`Composite: ${this.nome}`);
+        this.eventos.forEach((evento) => evento.exibirDetalhes());
+    }
+}
+*/
